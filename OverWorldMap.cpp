d97a1c103f05aa0dc188619ea810b7228cae16c8
Henry Bryant <henrythomasbryant@gmail.com>
2024-08-10 17:30:32 -0500

    cleaned up Overworldmap code

--- OverWorldMap.cpp
+++ OverWorldMap.cpp
@@ -1,54 +1,31 @@
 #include "OverWorldMap.h"
 #include "iostream"
+
+void OverWorldMap::initOverWorldMap()
+{
+	loadTextures();
+	initObjectTileMap();
+	initJsonReader();
+	loadMapFromJson();
+}
+
 void OverWorldMap::render(sf::RenderWindow* window)
 {
-
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 29; j++) {
 			if (tileMap[i][j] == 1) {
 				objectTileMap[i][j].setTexture(&grassTexture, false);
 			}
 			else if (tileMap[i][j] == 2) {
-				objectTileMap[i][j].setTexture(&stoneTexture,false);
-			}
-			else if (tileMap[i][j] == 'd') {
-				objectTileMap[i][j].setTexture(&grassTexture, false);
+				objectTileMap[i][j].setTexture(&stoneTexture, false);
 			}
 			window->draw(objectTileMap[i][j]);
 		}
 	}
 }
 
-void OverWorldMap::initOverWorldMap()
+void OverWorldMap::loadTextures() 
 {
-	loadTextures();
-	initMapArray();
-	mapPosX = 0;
-	mapPosY = 0;
-	sf::RectangleShape tile;
-	tile.setFillColor(sf::Color(200, 200, 200));
-	tile.setSize(sf::Vector2f(tileSize, tileSize));
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 29; j++) {
-			tile.setPosition(sf::Vector2f(10 + (j * tileSize), 40 + (i * tileSize)));
-			objectTileMap[i][j] = tile;
-		}
-	}
-
-	std::ifstream file("TileMap/MapTest.json");
-	reader.parse(file, actualJson);
-	std::cout << "layers: \n" << actualJson["layers"][0]["data"][0] << "\n";
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 29; j++) {
-			tileMap[i][j] = (char)actualJson["layers"][0]["data"][j + (i* 58)].asInt();
-		}
-	}
-
-	
-
-}
-
-void OverWorldMap::loadTextures() {
 	if (!stoneTexture.loadFromFile("./Graphics/Tiles/stoneTile.png"))
 	{
 		printf("Error Loading stoneTile.png\n");
@@ -58,130 +35,64 @@ void OverWorldMap::loadTextures() {
 	{
 		printf("Error Loading grassTile.png\n");
 	}
-
-	if (!doorTexture.loadFromFile("./Graphics/Tiles/doorTile.png"))
-	{
-		printf("Error Loading doorTile.png\n");
-	}
 }
 
-void OverWorldMap::initMapArray()
+void OverWorldMap::initJsonReader()
 {
-	for (int k = 0; k < 15; k++) {
-		for (int l = 0; l < 29; l++) {
-			mapArray[0][0][k][l] = map1[k][l];
-		}
-	}
-	for (int k = 0; k < 15; k++) {
-		for (int l = 0; l < 29; l++) {
-			mapArray[1][0][k][l] = map2[k][l];
-		}
-	}
-	for (int k = 0; k < 15; k++) {
-		for (int l = 0; l < 29; l++) {
-			mapArray[0][1][k][l] = map3[k][l];
-		}
-	}
-	for (int k = 0; k < 15; k++) {
-		for (int l = 0; l < 29; l++) {
-			mapArray[1][1][k][l] = map4[k][l];
-		}
-	}
-	for (int k = 0; k < 15; k++) {
-		for (int l = 0; l < 29; l++) {
-			tileMap[k][l] = mapArray[0][0][k][l];
-		}
-	}
-
-
-
+	std::ifstream file("TileMap/MapTest.json");
+	reader.parse(file, actualJson);
+	mapWidth = actualJson["layers"][0]["width"].asInt();
+	mapHeight = actualJson["layers"][0]["height"].asInt();
 }
 
-int OverWorldMap::getTileSize()
+
+void OverWorldMap::initObjectTileMap()
 {
-	return tileSize;
+	sf::RectangleShape tile;
+	tile.setFillColor(sf::Color(200, 200, 200));
+	tile.setSize(sf::Vector2f(tileSize, tileSize));
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 29; j++) {
+			tile.setPosition(sf::Vector2f(10 + (j * tileSize), 40 + (i * tileSize)));
+			objectTileMap[i][j] = tile;
+		}
+	}
 }
 
 void OverWorldMap::loadMapLeft()
 {
 	mapX -= 1;
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 29; j++) {
-			tileMap[i][j] = (char)actualJson["layers"][0]["data"][(j + (mapX * 29)) + (i * 58) + (mapY * ((30 * 58) / 2))].asInt();
-		}
-	}
-
-	/*
-	if (mapPosX != 0) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 29; j++) {
-				tileMap[i][j] = mapArray[mapPosX - 1][mapPosY][i][j];
-			}
-		}
-		mapPosX -= 1;
-		newMapLoaded = true;
-	}*/
+	loadMapFromJson();
 }
 
 void OverWorldMap::loadMapRight()
 {
-
 	mapX += 1;
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 29; j++) {
-			tileMap[i][j] = (char)actualJson["layers"][0]["data"][(j + (mapX * 29)) + (i * 58) + (mapY * ((30 * 58) / 2))].asInt();
-		}
-	}
-	/*
-	if (mapPosX != 1) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 29; j++) {
-				tileMap[i][j] = mapArray[mapPosX + 1][mapPosY][i][j];
-			}
-		}
-		mapPosX += 1;
-		newMapLoaded = true;
-	}*/
+	loadMapFromJson();
 }
 
 void OverWorldMap::loadMapUp()
 {
 	mapY -= 1;
-
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 29; j++) {
-			tileMap[i][j] = (char)actualJson["layers"][0]["data"][(j + (mapX * 29)) + (i * 58) + (mapY * ((30 * 58) / 2))].asInt();
-		}
-	}
-	/*
-	if (mapPosY != 0) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 29; j++) {
-				tileMap[i][j] = mapArray[mapPosX][mapPosY - 1][i][j];
-			}
-		}
-		mapPosY -= 1;
-		newMapLoaded = true;
-	}*/
+	loadMapFromJson();
 }
 
 void OverWorldMap::loadMapDown()
 {
 	mapY += 1;
+	loadMapFromJson();
+}
+
+void OverWorldMap::loadMapFromJson()
+{
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 29; j++) {
-			tileMap[i][j] = (char)actualJson["layers"][0]["data"][(j + (mapX * 29)) + (i * 58) + (mapY * ((30* 58) / 2)) ].asInt();
+			tileMap[i][j] = (char)actualJson["layers"][0]["data"][(j + (mapX * (mapWidth / 2))) + (i * mapWidth) + (mapY * ((mapHeight * mapWidth) / 2))].asInt();
 		}
 	}
+}
 
-	/*
-	if (mapPosY != 1) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 29; j++) {
-				tileMap[i][j] = mapArray[mapPosX][mapPosY + 1][i][j];
-			}
-		}
-		mapPosY += 1;
-		newMapLoaded = true;
-	}*/
+int OverWorldMap::getTileSize()
+{
+	return tileSize;
 }
