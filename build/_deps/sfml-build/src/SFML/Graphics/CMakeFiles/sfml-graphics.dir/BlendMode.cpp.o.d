6950f316aed18a931351d910c26736f049a0a62a
Henry Bryant <henrythomasbryant@gmail.com>
2025-02-15 21:13:32 -0600

    Removing dependencies and build folder

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o: \
- /mnt/c/Users/henry/source/2025Repos/AdventureGame2025/Adventure-Game/build/_deps/sfml-src/src/SFML/Graphics/BlendMode.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/henry/source/2025Repos/AdventureGame2025/Adventure-Game/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- /mnt/c/Users/henry/source/2025Repos/AdventureGame2025/Adventure-Game/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /mnt/c/Users/henry/source/2025Repos/AdventureGame2025/Adventure-Game/build/_deps/sfml-src/include/SFML/Config.hpp
